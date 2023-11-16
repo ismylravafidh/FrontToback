45c2035c089c56bbaf7fd75819a2a96e20a8f149
ismylravafidh <bd82v6x9y@code.edu.az>
2023-11-16 12:12:24 +0400

    Proje dosyası ekle.

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+
+
+            List<Product> productsAdd = new List<Product>();
+            productsAdd.Add(new Product()
+            {
+                Id = 1,
+                Name = "Qalos",
+                Price = 6.49,
+                İmgUrl = "indir (3).jpeg"
+            }); 
+            productsAdd.Add(new Product()
+            {
+                Id = 2,
+                Name = "Sort",
+                Price = 10,
+                İmgUrl = "images (1).jpeg"
+            }); 
+            productsAdd.Add(new Product()
+            {
+                Id = 3,
+                Name = "Qalos",
+                Price = 19.99,
+                İmgUrl = "indir (2).jpeg"
+            });
+            productsAdd.Add(new Product()
+            {
+                Id= 4,
+                Name = "Sort",
+                Price=9.99,
+                İmgUrl = "indir (1).jpeg"
+            });
+
+
+
+            return View(productsAdd);
+        }
+    }
+}
---
+++ WebApplication2/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication2.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string İmgUrl { get; set; }
+    }
+}
